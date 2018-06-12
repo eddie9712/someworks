c6094d75a768e679fb7da7c1e9c80f312f4793ea
eedie9712 <qw1562435@gmail.com>
2018-06-12 14:19:36 +0800

    test

--- loginsystem/public/index.js
+++ loginsystem/public/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
-    {
-         
+  {
+
       $('#register').click(function()
     {     
       $(".container").animate(
@@ -14,9 +14,54 @@ $(document).ready(function()
         $('.register_mode').animate(
             {
               'opacity':'1',
-              'top':'30%'
+              'top':'30%',
+              'height':'40vh'
               
             },1000,)});
       
-        });   
-      });   
+        }); 
+      $("#back").click(function()
+      {
+        $('.register_mode').animate(
+       {
+          'opacity':'0',
+          'top':'70%',
+          'height':'0%'
+      
+       },1000,
+     function()  
+     {
+       $('.container').animate(
+      {
+        'opacity':1,
+        'top':'30%',
+        'height':'40vh'
+      },1000,)})    
+            
+            });  //end of click function
+      }); 
+*$('#create').click(function()
+  {
+    event.preventDefault();  
+    
+ $.ajax({
+            method: "get",
+            url: "./ajax",
+            data: {
+            user:$("#create_data input[name=user]").val(),
+            account:$("create_data input[name=account]").val(),
+            password:$("create_data input[name=password]").val()
+            },
+            success: function(data) {
+                $("#ajax_content").text(data)
+            }
+          })*/
+
+
+
+
+
+
+
+
+
--- loginsystem/server.js
+++ loginsystem/server.js
@@ -1,1 +1,34 @@
-jweifoew
+const express=require('express');
+const app=express();
+const port=10065;
+app.listen(port);
+var mysql=require('mysql');
+app.use(express.static(__dirname + '/public'));
+var con = mysql.createConnection({    //to connect the database
+  host: "localhost",
+  user: "uidd2018_groupG",
+  password: "webGGdata",
+  database: "uidd2018_groupG"
+});
+app.get('/ajax',function(req,res)  // to create the data of user
+    {  
+       
+    
+    
+    
+    });
+
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
+
+
