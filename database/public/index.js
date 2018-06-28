372512b2d0025c166847c6454634c85207ef30b2
eddie9712 <qw1562435@gmail.com>
2018-06-28 12:29:14 +0800

    new

--- database/public/index.js
+++ database/public/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function()
 {
-  $('#pass button').click(function()
+  
+  $('#pass button').click(function(event)
  {
+    event.preventDefault(); 
    $.ajax({
     method:'get',
     url:"./get_data",
@@ -9,8 +11,20 @@ $(document).ready(function()
       user:$('#pass input[name="user"]').val(),
       std:$('#pass input[name="std"]').val(),
       department:$('#pass input[name="department"]').val()
-   }
+   },
+   success:function(data)
+  {
+     if(data==1)
+     {
+     $('#check').show();
+      }
+      else if(data==2)
+     {
+       $('#check2').show();
+     }
+     $('.container').hide();  
+   } 
     }); //end ajax
    });   //end click function
-
+    
 }) // end ready function
