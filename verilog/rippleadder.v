fc3d37e6744cc514da4844d22c2d4ae890163e3c
eddie9712 <qw1562435@gmail.com>
2018-07-01 15:55:21 +0800

    VERILOG

--- verilog/rippleadder.v
+++ verilog/rippleadder.v
@@ -9,7 +9,16 @@ half_adder h1(s1,c1,x,y);
 half_adder h2(s,c2,s1,z);
 or g1(c,c2,c1); 
 endmodule
-module 4bits(output [0:3]sum,output c4,input [3:0]a,input [3:0]b,input c0);
+
+
+
+module adder(
+  output[3:0] sum,
+  output      c4,
+  input[3:0]  a,b,
+  input        c0
+);
+
 wire c1,c2,c3;
 full_adder f1(sum[0],c1,a[0],b[0],c0),
             f2(sum[1],c2,a[1],b[1],c1),
