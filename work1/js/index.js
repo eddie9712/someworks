47985ee70a26a22cee13892e1d75f5e53715ec50
eedie9712 <qw1562435@gmail.com>
2018-07-28 20:23:12 +0800

    new

--- work1/js/index.js
+++ work1/js/index.js
@@ -1,1 +1,15 @@
-document
+$(document).ready(function()
+{
+   $("#provide").click(function()
+  {
+    $("#first").show();     
+     $("#second").show(); 
+     $("#third").show(); 
+     $("#change").html("<div class="col s5 push-s3" style="left:25%"><a class="waves-effect waves-light btn-small white red-text text-darken-3" style="width:30vw" id="finish">完成評分</a></div>")
+  });
+  $('#finish').click(function()
+  { 
+    
+
+  });  
+});  //end ready function
