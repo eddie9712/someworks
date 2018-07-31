a7e99a65d82bd495588db428723479278f00523a
eedie9712 <qw1562435@gmail.com>
2018-07-31 21:21:13 +0800

    new

--- work1/js/index.js
+++ work1/js/index.js
@@ -1,15 +1,39 @@
+var value1=0;
+var value2=0;
+var value3=0;
 $(document).ready(function()
 {
    $("#provide").click(function()
   {
-    $("#first").show();     
+     $("#first").show();     
      $("#second").show(); 
-     $("#third").show(); 
-     $("#change").html("<div class="col s5 push-s3" style="left:25%"><a class="waves-effect waves-light btn-small white red-text text-darken-3" style="width:30vw" id="finish">完成評分</a></div>")
+     $("#third").show();
+     $("#finish").show();
+     $("#provide").hide();
+     
   });
-  $('#finish').click(function()
+   $('#finish').click(function()
   { 
-    
-
-  });  
+    var myPromise= new Promise((resolve,reject)=>{
+      value1=$('#range1').val();
+       value2=$('#range2').val();
+        value3=$('#range3').val();
+        resolve("sucess");
+    });
+    myPromise.then((sucess)=>{
+     $('.cardtitle1').text(value1);
+     $('.cardtitle2').text(value2);
+     $('.cardtitle3').text(value3);
+     $('#changeTotext').html("恭喜成為第23位訪客。<br>你的付出，讓成大變得更好");
+     $('#finish').html('<a class="waves-effect waves-light btn-small white red-text text-darken-3" style="width:35vw">順便寫心得</a>');
+     $('#change>div').css("left","23%");
+     $('.next').show();
+    });    
+  }); //end click "finish" 
+   $('.next').click(function()
+    {
+      $('#change').hide();
+      $('#changeTotext').hide();
+      $('.next').html('');
+    });
 });  //end ready function
