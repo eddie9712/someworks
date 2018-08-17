b54ca56aa76d89094d46b1f13a905cbe98633311
eedie9712 <qw1562435@gmail.com>
2018-08-17 16:59:39 +0800

    new

--- work1/js/search.js
+++ work1/js/search.js
@@ -4,10 +4,21 @@ $(document).ready(function()
   {  
     window.location.replace('./index.html');   
      });
-   $()
-    
-    
-    
-    
+ $('#clear').click(function()
+    {   
+      $('#search').val('');
+      $('#block1').hide();
+       $('#block2').hide(); 
+     });
+   $('input').keypress(function(e) {
+    code = e.keyCode ? e.keyCode : e.which; // in case of browser compatibility
+    if(code == 13) {
+        e.preventDefault();
+        // do something
+        /* also can use return false; instead. */
+        }
+    }); 
+
+
     
     });  //end ready
