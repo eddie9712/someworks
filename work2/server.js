e23db1684566556ee96dc95a56a72596b2f1e67c
eddie <qw1562435@gmail.com>
2018-12-07 09:03:10 +0800

    request

--- work2/server.js
+++ work2/server.js
@@ -1,5 +1,27 @@
 const express=require("express")
 const app=express()
-const port=10065
+const port=3000
+const request = require('request');
+var course;
+var allinfo; 
 app.use(express.static(__dirname + '/public2'))
-app.listen(10065)
+app.listen(3000)
+app.get('/hi',function(req,res)
+{
+  var options = { 
+   method: 'GET',
+    url: 'https://nckuhub.com/test/course',
+    };
+   request(options,function(error,response,body) {
+   if(error) 
+      throw new Error(error);
+      course=response.body;
+     var obj = JSON.parse(course);
+      console.log(obj.courses[0]["課程名稱"]);
+      var obj1=Object.entries(obj)[0];
+      //console.log(Object.entries(obj1)[0]);
+      console.log(Object.keys(obj1[1]).length);  //the amount of the courses
+      allinfo=obj1[1];
+      res.send(allinfo)
+     });
+})
